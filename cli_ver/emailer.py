80c004f4b436401fb262f14c0cebb20f0bce4000
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-02-21 14:05:05 -0800

    Implemented email sending

--- cli_ver/emailer.py
+++ cli_ver/emailer.py
@@ -1,8 +1,32 @@
+import imghdr
 import os
+import smtplib
+from email.message import EmailMessage
 
-password = os.getenv("MOTION_DETECTOR_PASS")
-print(password)
+PASSWORD = os.getenv("MOTION_DETECTOR_PASS")
+SENDER = "ktjanzen42@gmail.com"
+RECEIVER = "ktjanzen42@gmail.com"
 
 
-def send_email():
-    print("Email was sent")
+def send_email(img_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "Newcomer Detected"
+    email_message.set_content("We have company")
+
+    with open(img_path, "rb") as file:
+        content = file.read()
+
+    email_message.add_attachment(content,
+                                 maintype="image",
+                                 subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
+
+if __name__ == "__main__":
+    send_email("image.png")
--- cli_ver/main.py
+++ cli_ver/main.py
@@ -52,7 +52,7 @@ while True:
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        send_email(img_with_obj)
 
     cv2.imshow("Video", frame)
 
